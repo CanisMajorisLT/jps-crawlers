70ac593e8804eedfb0a0c1530d696d4ae834ce58
Vytenis Butkevičius <vytenis.butkevicius@adform.com>
2016-02-28 16:05:45 +0200

    bring back tests

---
+++ test/cvb_spec/parser_spec.js
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+import fs from 'fs'
+import * as parser from '../../src/crawlers/cvb/parser';
+
+describe("Cvb parser", () => {
+
+    let fullPageHtml;
+
+    before((done)=> {
+        fs.readFile(__dirname + '/fullPage.html', (error, html)=> {
+            fullPageHtml = html.toString();
+            done()
+        })
+    });
+
+    describe("extractTotalPageCount", () => {
+        it("finds total number of ad listing pages", () => {
+
+            const result = parser.extractTotalPageCount(fullPageHtml);
+
+            expect(result).to.equal(55)
+        });
+
+        it("returns custom error when can't find value at his path", () => {
+            const html = '<div></div>';
+
+            expect(()=> {
+                parser.extractTotalPageCount(html)
+            }).to.throw(); // TODO check name of error
+        })
+    });
+
+    describe("extractFrontInfo", () => {
+        it("returns an array of extracted data", () => {
+            const result = parser.extractFrontInfo(fullPageHtml, {pageNumber: 0});
+            expect(result).to.be.an('array').with.length(52);
+
+        });
+/*
+        console.log(fullPageHtml); //TODO
+        const result = parser.extractFrontInfo(fullPageHtml);
+
+        result.forEach((r)=> {
+            it("each data object has 6 properties", () => {
+                   //expect(r).to.have.properties(['id'])
+            })
+        })*/
+
+    })
+
+});
