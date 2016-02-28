43bd8068a313206fc5055a4680ccc4e2bb6b29fc
Vytenis Butkevičius <vytenis.butkevicius@adform.com>
2016-02-28 17:51:42 +0200

    add loggly

--- logging/logger.js
+++ logging/logger.js
@@ -1,4 +1,5 @@
 var winston = require('winston');
+require('winston-loggly');
 
 module.exports = new (winston.Logger)({
    transports: [
@@ -28,6 +29,12 @@ module.exports = new (winston.Logger)({
            level: "info",
            maxFiles: 1,
            maxsize: 6553600
+       }),
+       new (winston.transports.Loggly)({
+           token: "212fde71-0c4c-4ed1-b133-08f7b2e1b2b0", // FIXME perkelti visa webini configa imongo, galiam fallbackinti i rc
+           subdomain: "vytkbutke",
+           tags: ["Winston-NodeJS"],
+           json:true
        })
    ]
 });
